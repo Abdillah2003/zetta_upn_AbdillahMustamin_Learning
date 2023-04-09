f68d07b030b61038151587a7d44a46e815b59441
Abdillah Mustamin <abdilamustamin@gmail.com>
2023-04-09 13:18:05 +0700

    push js zetta day 1

--- jsday1.js
+++ jsday1.js
@@ -1,26 +1,24 @@
-function bookPurchasing(buku, diskon, pajak)
-{
-    let hasil = 0;
-    if(buku && buku.harga)
-    {
-        const harga = buku.harga;
-        const totalDiskon = harga *(diskon/100);
-        const hargaSetelahDiskon = harga - totalDiskon;
-        const Tpajak = hargaSetelahDiskon * (pajak/100);
-        const hargaSetelahPajak = hargaSetelahDiskon - Tpajak;
-        
-
-        console.group();
-        console.log('Harga Buku : RP.', harga);
-        console.log(`Presentase Diskon : `, diskon,'%');
-        console.log('Presentase Pajak : ', pajak,'%');
-        console.log('Harga Setelah Diskon : Rp.', hargaSetelahDiskon);
-        console.log(`Harga Pajak : Rp. `, Tpajak);
-        console.log('Total Setelah Pajak : Rp.', hargaSetelahPajak);
-
-        console.groupEnd();
-
-    }
-    return hasil;
+function BookPurchasing({name,price,category},discount,tax) {
+    const amountOfDiscount = price*(discount/100);
+    const priceAfterDiscount = price - amountOfDiscount;
+    const amountOfTax = priceAfterDiscount * (tax/100);
+    const priceAfterTax = priceAfterDiscount - amountOfTax;
+    
+    return {
+        name: name,
+        price: price,
+        category: category,
+        amountOfDiscount: amountOfDiscount,
+        priceAfterDiscount: priceAfterDiscount,
+        amountOfTax: amountOfTax,
+        priceAfterTax: priceAfterTax
+    };
 }
-bookPurchasing({judul : 'Cerita Pulau Muna Sulawesi Tenggara', harga: 10000, printing_status: true}, 20, 10);
+        
+const book = {
+    "name": "Buku saku",
+    "category": "technology",
+    "price": 100000
+}
+
+console.log(BookPurchasing(book,10,10))
